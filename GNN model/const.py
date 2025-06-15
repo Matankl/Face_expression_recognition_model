bd694743c66e085dfaa4e89e36769fb3edbe1ce9
matankl <matankl01@gmail.com>
2025-06-15 17:14:55 +0300

    the training is all good, optuna run is ok the best acc is 38% for now, also the F1

--- GNN model/const.py
+++ GNN model/const.py
@@ -1,10 +1,10 @@
 
 
 DATA_DIR = r"C:\Users\matan\Desktop\Code\DataSets\Face_expression_recognition"
-OUT_DIR = r"C:\Users\matan\Desktop\Code\Face_expression_recognition_model\Trained GNN models"
+OUT_DIR = r"/GNN model/Trained_GNN_models"
 BATCH_SIZE = 64
 WORKERS_NUM = 1
 K_NEAREST_NEIGHBOR = 8
 EPOCHS = 30
-lr = 1e-3
+lr = 2e-2
 HIDDEN = 64
